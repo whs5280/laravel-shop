c7a85368a4f72a74204e28b07b5d3d082cb4d178
whs5280 <13434118350@163.com>
2019-06-09 22:14:23 +0800

    添加收获地址

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ Route::get('/', 'PagesController@root')->name('root')->middleware('verified');;
 //Auth用户认证
 Auth::routes(['verify' => true]);
 
+Route::group(['middleware' => ['auth', 'verified']], function (){
+
+    Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+});
