9b9a1540352579f30072654c13309726f15c6f95
whs5280 <13434118350@163.com>
2019-06-12 22:58:50 +0800

    首页-商品列表-查询排序

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -7,10 +7,43 @@ use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $products = Product::query()->where('on_sale', true)->paginate(12);
+        $search = $request->input('search', '');
+        $order = $request->input('order', '');
 
-        return view('products.index', ['products' => $products]);
+        $builder = Product::query()->where('on_sale', true);
+
+        if($search) {
+            $like = '%'.$search.'%';
+            $builder->where(function ($query) use ($like){
+                $query->where('title', 'like', $like)
+                    ->orWhere('description', 'like', $like)
+                    ->orWhereHas('skus', function ($query) use ($like) {
+                        $query->where('title', 'like', $like)
+                            ->orWhere('description', 'like', $like);
+                    });
+            });
+        }
+
+        if($order) {
+            // 是否是以 _asc 或者 _desc 结尾
+            if (preg_match('/^(.+)_(asc|desc)$/', $order, $m)) {
+                // 如果字符串的开头是这 3 个字符串之一，说明是一个合法的排序值
+                if (in_array($m[1], ['price', 'sold_count', 'rating'])) {
+                    // 根据传入的排序值来构造排序参数
+                    $builder->orderBy($m[1], $m[2]);
+                }
+            }
+        }
+
+        $products = $builder->paginate(12);
+
+        return view('products.index', ['products' => $products ,
+            'filters'  => [
+                'search' => $search,
+                'order'  => $order,
+            ],
+        ]);
     }
 }
