30df2644d3ce40b21296d4544120fa6f64604152
whs5280 <13434118350@163.com>
2019-06-15 23:22:54 +0800

    加入购物车

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\AddCartRequest;
+use App\Models\CartItem;
+
+class CartController extends Controller
+{
+    //
+    public function add(AddCartRequest $request)
+    {
+        $user = $request->user();
+        $skuId = $request->input('sku_id');
+        $amount = $request->input('amount');
+
+        // 从数据库中查询该商品是否已经在购物车中
+        $cart = $user->cartItems()->where('product_sku_id',$skuId)->first();
+
+        if($cart){
+            $cart->update([
+                'amount' => $cart->amount + $amount,
+            ]);
+        }else{
+            $cart = new CartItem(['amount' => $amount]);
+            $cart->user()->associate($user);
+            $cart->productSku()->associate($skuId);
+            $cart->save();
+        }
+
+        return [];
+    }
+}
