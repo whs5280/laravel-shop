c0ea41d23c44c244325fe43878db2244ce1c90f8
whs5280 <13434118350@163.com>
2019-06-29 17:38:02 +0800

    订单列表

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Exceptions\InvalidRequestException;
 use App\Http\Requests\OrderRequest;
+use App\Http\Requests\Request;
 use App\Jobs\CloseOrder;
 use App\Models\Order;
 use App\Models\ProductSku;
@@ -71,5 +72,17 @@ class OrdersController extends Controller
 
         $this->dispatch(new CloseOrder($order , config('app.order_ttl')));
         return $order;
+    }
+
+
+    public function index(Request $request)
+    {
+        $orders = Order::query()
+            ->with(['items.product'] , ['items.productSku'])
+            ->where('user_id' , $request->user()->id)
+            ->orderBy('created_at', 'desc')
+            ->paginate(12);
+
+        return view('orders.index', ['orders' => $orders]);
     }
 }
