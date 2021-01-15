dd5a26dfef6b827b007f628fb4aaf4bc7dc07a82
whs5280 <13434118350@163.com>
2021-01-15 17:31:13 +0800

    管理后台 - 新增、修改、删除优惠券

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -13,14 +13,23 @@ Route::group([
     $router->get('/', 'HomeController@index');
     $router->get('users', 'UsersController@index');
 
+    // 商品
     $router->get('products','ProductsController@index');
     $router->get('products/create', 'ProductsController@create');
     $router->post('products', 'ProductsController@store');
     $router->get('products/{id}/edit', 'ProductsController@edit');
     $router->put('products/{id}', 'ProductsController@update');
+
+    // 订单
     $router->get('orders', 'OrdersController@index')->name('admin.orders.index');
     $router->get('orders/{order}', 'OrdersController@show')->name('admin.orders.show');
     $router->post('orders/{order}/ship', 'OrdersController@ship')->name('admin.orders.ship');
 
+    // 优惠券
     $router->get('coupon_codes', 'CouponCodesController@index');
+    $router->post('coupon_codes', 'CouponCodesController@store');
+    $router->get('coupon_codes/create', 'CouponCodesController@create');
+    $router->get('coupon_codes/{id}/edit', 'CouponCodesController@edit');
+    $router->put('coupon_codes/{id}', 'CouponCodesController@update');
+    $router->delete('coupon_codes/{id}', 'CouponCodesController@destroy');
 });
