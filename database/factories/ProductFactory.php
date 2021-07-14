de50b107d9d8506bc6f0ebc5c20e5c037f2068fb
whs5280 <13434118350@163.com>
2021-07-14 16:27:34 +0800

    商品后台添加类目字段

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,6 +4,9 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Models\Product::class, function (Faker $faker) {
 
+    // 从数据库中随机取一个类目
+    $category = \App\Models\Category::query()->where('is_directory', false)->inRandomOrder()->first();
+
     $image = $faker->randomElement([
         "https://iocaffcdn.phphub.org/uploads/images/201806/01/5320/7kG1HekGK6.jpg",
         "https://iocaffcdn.phphub.org/uploads/images/201806/01/5320/1B3n0ATKrn.jpg",
@@ -26,6 +29,8 @@ $factory->define(App\Models\Product::class, function (Faker $faker) {
         'sold_count'   => 0,
         'review_count' => 0,
         'price'        => 0,
+        // 如果数据库中没有类目则 $category 为 null，同样 category_id 也设成 null
+        'category_id'  => $category ? $category->id : null,
     ];
 
 });
