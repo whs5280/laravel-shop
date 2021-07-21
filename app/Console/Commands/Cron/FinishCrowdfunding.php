400cddad4eb62b1713d2659cfb6eb0b42a2a9ccc
whs5280 <13434118350@163.com>
2021-07-21 15:47:00 +0800

    众筹结束逻辑

---
+++ app/Console/Commands/Cron/FinishCrowdfunding.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Console\Commands\Cron;
+
+use App\Jobs\RefundCrowdfundingOrders;
+use App\Models\CrowdfundingProduct;
+use App\Models\Order;
+use App\Services\OrderService;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class FinishCrowdfunding extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'cron:finish-crowdfunding';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '结束众筹';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        CrowdfundingProduct::query()
+            // 众筹结束时间早于当前时间
+            ->where('end_at', '<=', Carbon::now())
+            // 众筹状态为众筹中
+            ->where('status', CrowdfundingProduct::STATUS_FUNDING)
+            ->get()
+            ->each(function (CrowdfundingProduct $crowdfunding) {
+                // 如果众筹目标金额大于实际众筹金额
+                if ($crowdfunding->target_amount > $crowdfunding->total_amount) {
+                    // 调用众筹失败逻辑
+                    $this->crowdfundingFailed($crowdfunding);
+                } else {
+                    // 否则调用众筹成功逻辑
+                    $this->crowdfundingSucceed($crowdfunding);
+                }
+            });
+    }
+
+    protected function crowdfundingSucceed(CrowdfundingProduct $crowdfunding)
+    {
+        // 只需将众筹状态改为众筹成功即可
+        $crowdfunding->update([
+            'status' => CrowdfundingProduct::STATUS_SUCCESS,
+        ]);
+    }
+
+    protected function crowdfundingFailed(CrowdfundingProduct $crowdfunding)
+    {
+        // 将众筹状态改为众筹失败
+        $crowdfunding->update([
+            'status' => CrowdfundingProduct::STATUS_FAIL,
+        ]);
+        // 查询出所有参与了此众筹的订单
+        dispatch(new RefundCrowdfundingOrders($crowdfunding));
+    }
+}
