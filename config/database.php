958a40515aae2a63d524e9c25b0c4c12f192e60e
whs5280 <13434118350@163.com>
2021-07-22 18:07:54 +0800

    Laravel 版本升级、引入 Elasticsearch

--- config/database.php
+++ config/database.php
@@ -128,4 +128,11 @@ return [
 
     ],
 
+    /**
+     * ES config
+     */
+    'elasticsearch' => [
+        // Elasticsearch 支持多台服务器负载均衡，因此这里是一个数组
+        'hosts' => explode(',', env('ES_HOSTS')),
+    ]
 ];
