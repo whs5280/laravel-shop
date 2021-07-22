7f5a2a6e7dd0a19d180dd9c39c1277990258a043
whs5280 <13434118350@163.com>
2021-07-22 14:51:58 +0800

    商品添加长标题字段

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,7 +10,7 @@ class Product extends Model
     protected $fillable = [
         'title', 'description', 'image', 'on_sale',
         'rating', 'sold_count', 'review_count', 'price',
-        'type'
+        'type', 'long_title'
     ];
 
     protected $casts = [
